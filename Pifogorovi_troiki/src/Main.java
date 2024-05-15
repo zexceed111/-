68f3e92f9809a881b86a3c493a67843621689ae8
Kazurov Kostya <kazurovzxc@mail.ru>
2024-05-15 14:10:06 +0200

    Третий коммит в idea

--- Pifogorovi_troiki/src/Main.java
+++ Pifogorovi_troiki/src/Main.java
@@ -1,9 +1,13 @@
+
 class PiTriy {
 
     public static void main(String[] args) {
+
         if(args == null || args.length == 0){
             System.out.println("Введите количество пифагоровых троек для вывода или введите три числа для проверки.");
+
             return;
+
         }
         if(args.length == 1){ //вывести N троек
             int N = Integer.parseInt(args[0]);
